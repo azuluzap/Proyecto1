8a506c095e5be7082bbbe6c9289db38179e7cae9
azuluzap <azuluzap@gmail.com>
2024-08-27 22:50:30 -0500

    Second commit

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-// Función para encriptar el texto
-function encryptText() {
-    let inputText = document.getElementById("inputText").value;
-    let encryptedText = inputText
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
-    document.getElementById("outputText").value = encryptedText;
-}
-
-// Función para desencriptar el texto
-function decryptText() {
-    let inputText = document.getElementById("inputText").value;
-    let decryptedText = inputText
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
-    document.getElementById("outputText").value = decryptedText;
-}
-
-// Función para copiar el texto al portapapeles
-function copyText() {
-    let outputText = document.getElementById("outputText");
-    outputText.select();
-    document.execCommand("copy");
-    alert("Texto copiado al portapapeles");
-}
-
 function copyText() {
     let outputText = document.getElementById("inputText");
     outputText.select();
